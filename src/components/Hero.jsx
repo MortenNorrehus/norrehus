3fc2f0186358e263f283bbebdb28762ea91132b2
MortenNorrehus <mortennorrehus@gmail.com>
2022-09-21 14:41:12 +0200

    New Design and menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,23 @@
 import Button from "./Button";
 
 const Hero = () => {
+  const handleContact = (event) => {
+    const infoContainer = document.querySelector(".contact--container");
+
+    if (infoContainer.classList.contains("visible")) {
+      infoContainer.classList.add("fade-out");
+    } else {
+      infoContainer.classList.add("visible");
+      document.querySelector("html").style.overflow = "hidden";
+    }
+    infoContainer.addEventListener("transitionend", () => {
+      if (infoContainer.classList.contains("fade-out")) {
+        document.querySelector("html").style.overflow = "";
+        infoContainer.classList.remove("visible");
+        infoContainer.classList.remove("fade-out");
+      }
+    });
+  };
   return (
     <section className="hero front bg-black">
       <div className="content flex-col">
@@ -9,17 +26,22 @@ const Hero = () => {
           <span className="lastName">Nørrehus</span>
   </h1> {*/}
         <h1>Hey! jeg er freelance frontend udvikler</h1>
-
         {/*} <h2>Jeg laver ting til internettet</h2>
         <h3>
           <i>(Shopify, Liquid, JavaScript, React) </i>
 </h3> {*/}
-
         <span className="subtitle">
           Mit navn er Morten Nørrehus, og jeg hjælper virksomheder med kode,
           fede features og digital sparring der skaber konverteringer!
         </span>
         <Button text="Se hvad jeg har lavet" link="#cases" />
+        <button
+          className="button contact"
+          aria-label="toggle-menu"
+          onClick={handleContact}
+        >
+          eller kontakt mig
+        </button>
       </div>
       <div className="arrow"></div>
     </section>
